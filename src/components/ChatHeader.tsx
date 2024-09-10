060423e344328d18256a202dd0f6a561d8b2d01b
ivory-code <kyo3553@gmail.com>
2024-09-10 23:39:46 +0900

    feat: implement Auth

---
+++ src/components/ChatHeader.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import {Button} from '@/components/ui/button'
+import {supabaseBrowser} from '@/lib/supabase/browser'
+import {User} from '@supabase/supabase-js'
+import {useRouter} from 'next/navigation'
+
+interface Props {
+  user?: User
+}
+
+export default function ChatHeader({user}: Props) {
+  const router = useRouter()
+
+  const handleLoginWithGithub = () => {
+    const supabase = supabaseBrowser()
+    console.log(user, '????')
+    supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: location.origin + '/auth/callback',
+      },
+    })
+  }
+
+  const handleLogout = async () => {
+    const supabase = supabaseBrowser()
+
+    await supabase.auth.signOut()
+
+    router.refresh()
+  }
+
+  return (
+    <div className="h-20">
+      <div className="p-5 border-b flex items-center justify-between">
+        <div>
+          <h1 className="text-xl font-bold">Daily Chat</h1>
+          <div className="flex items-center gap-1">
+            <div className="h-4 w-4 bg-green-500 rounded-full animate-pulse"></div>
+            <h1 className="text-sm text-gray-400">2 onlines</h1>
+          </div>
+        </div>
+        {user ? (
+          <Button onClick={handleLogout}>Logout</Button>
+        ) : (
+          <Button onClick={handleLoginWithGithub}>Login</Button>
+        )}
+      </div>
+    </div>
+  )
+}
